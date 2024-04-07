dd6983d1b55343476b62948bfbf94e7b72d864d1
Aayushi-Ag <aayushiagarwal815@gmail.com>
2024-04-08 00:22:34 +0530

    updated version of code

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 9999;
 // Serve static files (HTML, CSS, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 // Redirect route for Spotify authorization
 app.get('/login', (req, res) => {
   const client_id = process.env.SPOTIFY_CLIENT_ID;
@@ -21,17 +20,31 @@ app.get('/login', (req, res) => {
   const scopes = 'user-read-private user-top-read playlist-modify-public playlist-modify-private';
   const state = ''; // Optionally, you can add state for security
 
-  //const authorizationUrl = `https://accounts.spotify.com/en/authorize?client_id=${client_id}&response_type=code&redirect_uri=http://localhost:9999/MenuPage.html&state=${state}&show_dialog=false&scope=${encodeURIComponent(scopes)}`;
-  const authorizationUrl = 'https://accounts.spotify.com/en/authorize?client_id=0ed9ce5473ad4212a584161201e9e7a8&response_type=code&redirect_uri=http://localhost:9999/MenuPage.html&state=&show_dialog=false&scope=user-read-private%20user-top-read%20playlist-modify-public%20playlist-modify-private'
+  const authorizationUrl = "https://accounts.spotify.com/en/authorize?client_id=${client_id}&response_type=code&redirect_uri=${encodeURIComponent(redirect_uri)}&state=${state}&show_dialog=false&scope=${encodeURIComponent(scopes)}";
 
   res.redirect(authorizationUrl);
 });
 
-
-
 // Serve MenuPage.html
 app.get('/MenuPage.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'MenuPage.html'));
+});
+
+// Route to handle fetching user's Spotify data
+app.get('/spotify-data', async (req, res) => {
+  try {
+    // Make requests to the Spotify API here to fetch user data
+    // For simplicity, let's just send sample data
+    const topTracks = ['Janam Janam - Pritam, Arijit Singh, Antara Mitra', 'Choo Lo - The Local train', 'Hona Tha Pyar - Atif Aslam, Hadiqa Kiani', 'Kaash Aisa Hota - Darshan Raval', 'Sadqay - Aashir Wajahat, NAYEL, Nehaal Naseem', 'Saware - Pritam, Arijit Singh', 'Husn - Anuv Jain', 'Get him Back! - Olivia Rodrigo', 'Dhokha Dhadi - Arijit Singh, Palak Muchhal, Pritam', 'Heeriye - Jasleen Royal, Arijit Singh'];
+    const topPlaylists = ['Filmi', 'Modern Bollywood', 'Pop', 'Desi Pop', 'Indian Lo-Fi'];
+    const topArtists = ['Aditya Rikhari', 'Pritam', 'Talwiinder', 'Olivia Rodrigo', 'Lana Del Rey'];
+
+    // Send the data as JSON response
+    res.json({ topTracks, topPlaylists, topArtists });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 app.listen(PORT, () => {
