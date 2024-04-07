a30cfafaab379d54fcc326ae47b32a9e31dac9b4
Aayushi-Ag <aayushiagarwal815@gmail.com>
2024-04-08 01:42:28 +0530

    updated auth url

--- server.js
+++ server.js
@@ -20,8 +20,7 @@ app.get('/login', (req, res) => {
   const scopes = 'user-read-private user-top-read playlist-modify-public playlist-modify-private';
   const state = ''; // Optionally, you can add state for security
 
-  const authorizationUrl = "https://accounts.spotify.com/en/authorize?client_id=${client_id}&response_type=code&redirect_uri=${encodeURIComponent(redirect_uri)}&state=${state}&show_dialog=false&scope=${encodeURIComponent(scopes)}";
-
+  const authorizationUrl = "https://accounts.spotify.com/en/authorize?client_id=0ed9ce5473ad4212a584161201e9e7a8&response_type=code&redirect_uri=http://localhost:9999/MenuPage.html&state=&show_dialog=false&scope=user-read-private%20user-top-read%20playlist-modify-public%20playlist-modify-private"
   res.redirect(authorizationUrl);
 });
 
